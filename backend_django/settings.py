d3cf2daa5b2fd5c9ca8872b18a6282e1f614c764
marcomag416 <marcomag416@gmail.com>
2025-11-07 16:54:55 +0100

    feat: external db

--- backend_django/settings.py
+++ backend_django/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 import os
 from pathlib import Path
 import environ
+import dj_database_url
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -101,6 +103,8 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+DATABASES['default'] = dj_database_url.parse(env('DATABASE_URL'))
 
 
 # Password validation
