c6573630d2938873c91d594da11dd673cd2bb67a
marcomag416 <marcomag416@gmail.com>
2025-11-24 14:11:28 +0100

    FEAT: new cors allow all policy

--- backend_django/settings.py
+++ backend_django/settings.py
@@ -70,6 +70,8 @@ MIDDLEWARE = [
 
 CORS_ALLOWED_ORIGINS = env.list('CORS_ALLOWED_ORIGINS', default=[])
 
+CORS_ALLOW_ALL_ORIGINS = env.bool('CORS_ALLOW_ALL_ORIGINS', default=False)
+
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'knox.auth.TokenAuthentication',
